dfd4cfa6e3dbdd6da952e894e7e7749a9cab4ba3
Combey Theo <theo.combey@student-cs.fr>
2018-11-19 14:35:24 +0100

    Added save and load functions

--- reseau_neuronnes/perceptron.py
+++ reseau_neuronnes/perceptron.py
@@ -21,13 +21,12 @@ class Perceptron:
     def set_weights_and_biases(self, weights, biases):
         """
         Sets the weights and the biases of the network
-        :param weights:
-        :param biases:
-        :return:
+        :param weights: the list of weights matrices
+        :param biases: the list of biases vectors
         """
 
-        self.weights = weights
-        self.biases = biases
+        self.weights = numpy.copy(weights)
+        self.biases = numpy.copy(biases)
 
     def randomize(self, low, high):
         """
@@ -53,4 +52,98 @@ class Perceptron:
         pass
 
 
+def save_network(network, path, separators=";,"):
+    """
+    Saves a neural network to a file
+    :param network: the network to save
+    :param path: the path to the file
+    :param separators: the list of separators used in the file
+    """
+
+    layers = network.layers
+    weights = network.weights
+    biases = network.biases
+
+    file = open(path, "w")
+
+    for k in range(len(layers)):
+        if k != 0:
+            file.write(separators[0])
+
+        file.write(str(layers[k]))
+
+    file.write("\n")
+
+    for k in range(len(weights)):
+        for i in range(weights[k].shape[0]):
+            if i != 0:
+                file.write(separators[0])
+
+            for j in range(weights[k].shape[1]):
+                if j != 0:
+                    file.write(separators[1])
+
+                file.write(str(weights[k][i, j]))
+
+        file.write("\n")
+
+    for k in range(len(biases)):
+        for i in range(biases[k].shape[0]):
+            if i != 0:
+                file.write(separators[0])
+
+            file.write(str(biases[k][i, 0]))
+
+        file.write("\n")
+
+    file.flush()
+    file.close()
+
+
+def load_network(path, separator=";,"):
+    """
+    Loads a neural network from a file
+    :param path: the path to file which contains the network
+    :param separator: the list of separators used in the file
+    :return: an instance of the loaded neural network
+    """
+
+    layers = []
+    weights = []
+    biases = []
+
+    file = open(path, "r")
+
+    layers_line = file.readline()
+
+    for size in layers_line.split(separator[0]):
+        layers.append(int(size))
+
+    for k in range(len(layers) - 1):
+        weight = numpy.zeros((layers[k+1], layers[k]), dtype=float)
+        line = file.readline()
+        nodes = line.split(separator[0])
+
+        for i in range(len(nodes)):
+            node = nodes[i].split(separator[1])
+
+            for j in range(len(node)):
+                weight[i, j] = float(node[j])
+
+        weights.append(weight)
+
+    for k in range(len(layers) - 1):
+        bias = numpy.zeros((layers[k+1], 1), dtype=float)
+        line = file.readline()
+        nodes = line.split(separator[0])
+
+        for i in range(len(nodes)):
+            bias[i, 0] = float(nodes[i])
+
+        biases.append(bias)
+
+    network = Perceptron(layers)
+    network.set_weights(weights, biases)
+
+    return network
 
