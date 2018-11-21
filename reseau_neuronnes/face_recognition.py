8ade36d3a3b78a936775fe94d8ebe9f13633aa75
Nicolas_LAUBE <nicolas.laube@student-cs.fr>
2018-11-21 11:01:22 +0100

    fonction de reconnaissance visages

--- reseau_neuronnes/face_recognition.py
+++ reseau_neuronnes/face_recognition.py
@@ -1,4 +1,6 @@
 import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def face_delimitation(face_path, padding):
@@ -14,23 +16,29 @@ def face_delimitation(face_path, padding):
     img = cv.imread(face_path)
     faces = face_cascade.detectMultiScale(img, 1.3, 5)
     for face in faces:
-        (x, y, w, h) = face
-        x_rescaled = x - padding
-        y_rescaled = y - 2 * padding
-        w_rescaled = w + 2 * padding
-        h_rescaled = h + 4 * padding
-        if x_rescaled >= 0 and y_rescaled >= 0 and w_rescaled <= len(img) and h_rescaled <= len(img[0]):
-            (x_beginning, y_beginning, face_width, face_height) = (x_rescaled, y_rescaled, w_rescaled, h_rescaled)
-        else:
-            (x_beginning, y_beginning, face_width, face_height) = (0, 0, len(img), len(img[0]))
+        (x_beginning, y_beginning, face_width, face_height) = face
         roi_img = img[y_beginning:y_beginning+face_height, x_beginning:x_beginning+face_width]
         eyes = eye_cascade.detectMultiScale(roi_img)
         noses = nose_cascade.detectMultiScale(roi_img)
+        #points = []
         for (ex_beginning, ey_beginning, eye_width, eye_height) in eyes:
+            #points.append((ex_beginning, ey_beginning))
+            #points.append((ex_beginning+eye_width, ey_beginning + eye_height))
             cv.rectangle(roi_img, (ex_beginning, ey_beginning), (ex_beginning+eye_width, ey_beginning+eye_height), (0, 255, 0), 2)
         for (nx_beginning, ny_beginning, nose_width, nose_height) in noses:
+            #points.append((nx_beginning, ny_beginning))
+            #points.append((nx_beginning + nose_width, ny_beginning + nose_height))
             cv.rectangle(roi_img, (nx_beginning, ny_beginning), (nx_beginning+nose_width, ny_beginning+nose_height), (0, 0, 255), 2)
+        #rows, cols = len(img), len(img[0])
+        #pts1 = np.float32(points)
+        #pts2 = np.float32([[0,0], [0, 0], [0, 0], [0, 0]])
+        #M = cv.getAffineTransform(pts1, pts2)
+        #dst = cv.warpAffine(img, M, (cols, rows))
+
+        #plt.subplot(121),plt.imshow(img), plt.title('Input')
+        #plt.subplot(122),plt.imshow(dst),plt.title('Output')
+        #plt.show()
         cv.imshow('img', roi_img)
     cv.waitKey(0)
     cv.destroyAllWindows()
-face_delimitation('../database/images/POUTINE_004.png', 50)
+face_delimitation('../database/images/MERKEL_003.png', 50)
